9d52bc0c721eaf770ca8f8a83a009a5be5ba7a69
joepickrell <joepickrell@gmail.com>
2014-02-21 16:50:31 -0500

    Adding configure

---
+++ config.h
@@ -0,0 +1,41 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* define if the Boost library is available */
+#define HAVE_BOOST /**/
+
+/* Define to 1 if you have the `gsl' library (-lgsl). */
+#define HAVE_LIBGSL 1
+
+/* Define to 1 if you have the `gslcblas' library (-lgslcblas). */
+#define HAVE_LIBGSLCBLAS 1
+
+/* Define to 1 if you have the `m' library (-lm). */
+#define HAVE_LIBM 1
+
+/* Define to 1 if you have the `z' library (-lz). */
+#define HAVE_LIBZ 1
+
+/* Name of package */
+#define PACKAGE "fgwas"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "joepickrell@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "fgwas"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "fgwas 0.03"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "fgwas"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://gwas.googlecode.com/"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.03"
+
+/* Version number of package */
+#define VERSION "0.03"
